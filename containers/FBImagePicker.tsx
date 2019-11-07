97902ddc1692564bff300c1c713077759d008098
William Hurley <wjhurley@comcast.net>
2019-11-07 07:30:35 -0800

    Store photo and grab metadata

--- containers/FBImagePicker.tsx
+++ containers/FBImagePicker.tsx
@@ -3,9 +3,14 @@ import { Button, Image, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import Constants from 'expo-constants';
 import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
+
+interface State {
+  image: MediaLibrary.AssetInfo | null;
+}
 
 export class FBImagePicker extends React.Component {
-  state = {
+  state: State = {
     image: null,
   };
 
@@ -16,6 +21,7 @@ export class FBImagePicker extends React.Component {
   getPermissionAsync = async () => {
     if (Constants.platform.ios) {
       const { status } = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
+
       if (status !== 'granted') {
         alert('Sorry, we need camera permissions to make this work!');
       }
@@ -23,6 +29,7 @@ export class FBImagePicker extends React.Component {
 
     if (Constants.platform.android) {
       const { status } = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
+
       if (status !== 'granted') {
         alert('Sorry, we need camera permissions to make this work!');
       }
@@ -30,30 +37,32 @@ export class FBImagePicker extends React.Component {
   };
 
   takePhoto = async () => {
-    let result = await ImagePicker.launchCameraAsync({
+    const result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: false,
       base64: true,
     });
 
-    console.log(result);
-
-    if (result.cancelled === false) {
-      this.setState({ image: result.uri });
+    if (result.cancelled === true) { // cancelled check needed so that TS knows the proper type of 'result'
+      console.log(JSON.stringify(result));
+      return;
     }
+
+    const asset = await MediaLibrary.createAssetAsync(result.uri);
+    const assetInfo = await MediaLibrary.getAssetInfoAsync(asset);
+    console.log(assetInfo);
+
+    this.setState({ image: asset });
   };
 
   render() {
     let { image } = this.state;
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-          <Button
-              title="Take photo"
-              onPress={this.takePhoto}
-          />
-        </View>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {image && <Image source={{ uri: image.uri }} style={{ width: 200, height: 200 }} />}
+        {!image && <Button title="Take photo" onPress={this.takePhoto} />}
+      </View>
     );
   }
 }
